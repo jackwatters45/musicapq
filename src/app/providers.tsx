86e955fb105375dccdc299ef3984c9f726a9598e
Jack Watters <johnwatt@usc.edu>
2024-06-03 23:14:55 -0700

    Add react query to privider.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import {
+	QueryCache,
+	QueryClient,
+	QueryClientProvider,
+} from "@tanstack/react-query";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { ThemeProviderProps } from "next-themes/dist/types";
 
@@ -7,15 +12,27 @@ function ThemeProvider({ children, ...props }: ThemeProviderProps) {
 	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
 
+const queryClient = new QueryClient({
+	queryCache: new QueryCache({
+		onError: (error, query) => {
+			if (query.meta?.errorMessage) {
+				// toast.error(query.meta.errorMessage);
+			}
+		},
+	}),
+});
+
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
-		<ThemeProvider
-			attribute="class"
-			defaultTheme="system"
-			enableSystem
-			disableTransitionOnChange
-		>
-			{children}
-		</ThemeProvider>
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider
+				attribute="class"
+				defaultTheme="system"
+				enableSystem
+				disableTransitionOnChange
+			>
+				{children}
+			</ThemeProvider>
+		</QueryClientProvider>
 	);
 }
